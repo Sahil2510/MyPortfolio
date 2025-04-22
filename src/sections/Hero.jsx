2d6d8774f8985c9a7137387ffd638a3fc91ac0d6
Sahil Chavan <61664145+Sahil2510@users.noreply.github.com>
2025-04-22 13:25:31 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         <p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
           Hi, I am Sahil <span className="waving-hand">👋</span>
         </p>
-        <p className="hero_tag text-gray_gradient">Inventing Tomorrow's AI</p>
+        <p className="hero_tag text-gray_gradient">Crafting the Future of AI - One Line of Code at a Time</p>
       </div>
 
       <div className="w-full h-full absolute inset-0">
